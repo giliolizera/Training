13388b424c66b3c64b6f0fe85836aab5ab29c86e
giliolizera <guilhermegilioli93@gmail.com>
2023-07-12 11:19:31 -0300

    feat: add disclosure

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,31 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="bg-slate-700">
+  <div class="w-full px-4 pt-16">
+    <div class="mx-auto w-full max-w-md rounded-2xl bg-slate-900 p-2">
+      <button @click="exibir = !exibir" type="button" class="flex w-full justify-between rounded-lg bg-slate-800 px-4 py-2 text-left text-sm font-medium text-purple-300 hover:bg-slate-700 focus:outline-none">
+        <span>What is your refund policy?</span>
+      </button>
+      <div v-if="exibir" class="px-4 pb-2 pt-4 text-sm text-gray-500">If you're unhappy with your purchase for any reason, email us within 90 days and we'll refund you in full, no questions asked.</div>
+        <div class="mt-2">
+          <button @click="exibir2 = !exibir2" class="flex w-full justify-between rounded-lg bg-slate-800 px-4 py-2 text-left text-sm font-medium text-purple-300 hover:bg-slate-700 focus:outline-none">
+            <span>Do you offer technical support?</span>
+          </button>
+          <div v-if="exibir2" class="px-4 pb-2 pt-4 text-sm text-gray-500">No.</div>
+        </div>
+    </div>
   </div>
+  </div>
+  <div class="bg-slate-700 w-full h-screen"></div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data:() => ({
+    exibir: false,
+    exibir2: false
+  }),
+  methods: {
+
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
