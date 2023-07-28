ad5d8b45359451c2ede959905cba30cecdc57979
giliolizera <guilhermegilioli93@gmail.com>
2023-07-28 10:24:35 -0300

    feat: style

--- src/components/Upside.vue
+++ src/components/Upside.vue
@@ -1,47 +1,47 @@
 <template>
-   <div class="flex justify-center max-wd-16 bg-opacity-30 bg-slate-600">
+   <div class="flex justify-start bg-slate-900">
       <router-link to="/">
-         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2">
-            Site
+         <button class="text-blue-600 bg-slate-900 font-medium text-3xl flex p-4 justify-start">
+            Training WebSite
          </button>
       </router-link>
       <router-link to="/disclosure">
-         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2">
+         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2 mt-3.5">
             Disclosure
          </button>
       </router-link> 
       <router-link to="/tabs">
-         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2">
+         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2 mt-3.5">
             Tabs
          </button>
       </router-link>
       <router-link to="/select">
-         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2">
+         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2 mt-3.5">
             Select
          </button>
       </router-link>
       <router-link to="/menu">
-         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2">
+         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2 mt-3.5">
          Menu
          </button>
       </router-link>
       <router-link to="/combobox">
-         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2">
+         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2 mt-3.5">
          Combobox
          </button>
       </router-link>
       <router-link to="/dialog">
-         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2">
+         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2 mt-3.5">
          Dialog
          </button>
       </router-link>
       <router-link to="/popover">
-         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2">
+         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2 mt-3.5">
          Popover
       </button>
       </router-link>
       <router-link to="/radio">
-         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2">
+         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2 mt-3.5">
          Radio
          </button>
       </router-link>
