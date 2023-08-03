db3c131e0ab6bab544815e4700c3e029dfd008bf
giliolizera <guilhermegilioli93@gmail.com>
2023-08-03 14:37:43 -0300

    farm

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,24 @@
 <template>
    <div class="w-full bg-slate-900">
       <router-link to="/">
-         <button class="text-blue-600 bg-slate-900 font-medium text-3xl flex p-4 justify-start">
+         <button class="text-blue-600 bg-slate-900 font-medium w-auto text-3xl flex p-4 justify-start">
             Training WebSite
          </button>
       </router-link>
    </div>
    <div class="bg-slate-800 grid grid-cols-1 gap-4 p-2 text-center lg:grid-cols-3 md:grid-cols-3">
-      <div class="col-span-1 mt-2 text-lg text-white font-medium">
+      <div class="col-span-1 mt-4 text-lg text-white font-medium">
          Junte-se conosco e descubra as vantagens em utilizar o Traning para gerenciar seus negócios!
       </div>
       <div class="col-span-2 grid lg:grid-cols-2 gap-4 pr-10 pl-2">
          <div>
-            <div class="text-white text-sm font-medium flex pl-1">
+            <div class="text-white text-sm font-medium flex pl-1 mt-2">
                Nome
             </div>
             <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md mt-1 pl-3 p-2" v-model="form.nome" >
          </div>
          <div>
-            <div class="text-white text-sm font-medium flex pl-1">
+            <div class="text-white text-sm font-medium flex pl-1 mt-2">
                Email
             </div>
             <input type="email" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 pl-3 mt-1" v-model="form.email">
@@ -35,10 +35,22 @@
             </div>
             <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1" v-model="form.cpf"  v-maska="'###.###.###-##'">
          </div>
+         <div>
+            <div class="text-white text-sm font-medium flex pl-1 mt-2">
+               Senha
+            </div>
+            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1" v-model="form.senha">
+         </div>
+         <div>
+            <div class="text-white text-sm font-medium flex pl-1 mt-2">
+               Confirmar Senha
+            </div>
+            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1" v-model="form.confirmarSenha">
+         </div>
          <div class="flex justify-end lg:col-span-2" v-if="exibir">
             <div>
                <router-link to="/disclosure">
-                  <button @click="validar()" class="bg-gray-200 hover:bg-gray-300 max-md:24 text-gray-900 font-medium text-sm py-2 px-6 rounded mt-3">
+                  <button @click="validar(), confereSenha()" class="bg-gray-200 hover:bg-gray-300 max-md:24 text-gray-900 font-medium text-sm py-2 px-6 rounded mt-3">
                         ENTRAR
                   </button>
                </router-link>
@@ -56,7 +68,9 @@ export default {
            nome: '',
            email: '',
            telefone: '',
-           cpf: ''
+           cpf: '',
+           senha: '',
+           confirmarSenha: ''
        },
        avançar: false,
        exibir: true
@@ -69,6 +83,15 @@ export default {
          }
          else {
             this.avançar = false
+         }
+      },
+      confereSenha(){
+         if(this.form.senha != this.form.confirmarSenha){
+            this.exibir = false
+         }
+         else {
+            this.exibir = true
+            console.log(this.exibir)
          }
       }
    },
