4c6387f6f2111de3284074d040f9095782023597
giliolizera <guilhermegilioli93@gmail.com>
2023-08-04 09:13:19 -0300

    feat: fuck

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -15,43 +15,50 @@
             <div class="text-white text-sm font-medium flex pl-1 mt-2">
                Nome
             </div>
-            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md mt-1 pl-3 p-2" v-model="form.nome" >
+            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md mt-1 pl-3 p-2"
+               v-model="form.nome">
          </div>
          <div>
             <div class="text-white text-sm font-medium flex pl-1 mt-2">
                Email
             </div>
-            <input type="email" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 pl-3 mt-1" v-model="form.email">
+            <input type="email" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 pl-3 mt-1"
+               v-model="form.email">
          </div>
          <div>
             <div class="text-white text-sm font-medium flex pl-1 mt-2">
                Telefone
             </div>
-            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1" v-model="form.telefone" v-maska="'(##) #####-####'">
+            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1"
+               v-model="form.telefone" v-maska="'(##) #####-####'">
          </div>
          <div>
             <div class="text-white text-sm font-medium flex pl-1 mt-2">
                CPF
             </div>
-            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1" v-model="form.cpf"  v-maska="'###.###.###-##'">
+            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1"
+               v-model="form.cpf" v-maska="'###.###.###-##'">
          </div>
          <div>
             <div class="text-white text-sm font-medium flex pl-1 mt-2">
                Senha
             </div>
-            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1" v-model="form.senha">
+            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1"
+               v-model="form.senha">
          </div>
          <div>
             <div class="text-white text-sm font-medium flex pl-1 mt-2">
                Confirmar Senha
             </div>
-            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1" v-model="form.confirmarSenha">
+            <input type="text" class="w-full text-gray-200 border-blue-600 border bg-slate-700 rounded-md p-2 mt-1"
+               v-model="form.confirmarSenha">
          </div>
-         <div class="flex justify-end lg:col-span-2" v-if="exibir">
+         <div class="flex justify-end lg:col-span-2">
             <div>
                <router-link to="/disclosure">
-                  <button @click="validar(), confereSenha()" class="bg-gray-200 hover:bg-gray-300 max-md:24 text-gray-900 font-medium text-sm py-2 px-6 rounded mt-3">
-                        ENTRAR
+                  <button @click="validar(), confereSenha()"
+                     class="bg-gray-200 hover:bg-gray-300 max-md:24 text-gray-900 font-medium text-sm py-2 px-6 rounded mt-3">
+                     ENTRAR
                   </button>
                </router-link>
             </div>
@@ -64,34 +71,42 @@
 <script>
 export default {
    data: () => ({
-       form: {
-           nome: '',
-           email: '',
-           telefone: '',
-           cpf: '',
-           senha: '',
-           confirmarSenha: ''
-       },
-       avançar: false,
-       exibir: true
+      form: {
+         nome: '',
+         email: '',
+         telefone: '',
+         cpf: '',
+         senha: '',
+         confirmarSenha: ''
+      },
+      avançar: false,
+      exibir: false,
    }),
-   methods:{
-      validar(){
-         if(this.form.nome != '' && this.form.email != '' && this.form.telefone != '' && this.form.cpf != ''){
+   methods: {
+      validar() {
+         if (this.form.nome != '' && this.form.email != '' && this.form.telefone != '' && this.form.cpf != '') {
             this.avançar = true
+            console.log('validar')
             console.log(this.avançar)
          }
          else {
             this.avançar = false
+            console.log('validar')
+            console.log(this.avançar)
          }
       },
-      confereSenha(){
-         if(this.form.senha != this.form.confirmarSenha){
+      confereSenha() {
+         if (this.form.senha != this.form.confirmarSenha || this.form.senha == '' || this.form.confirmarSenha == '') {
             this.exibir = false
+            console.log('senha')
+            console.log(this.exibir)
          }
          else {
-            this.exibir = true
-            console.log(this.exibir)
+            if (this.form.senha != '' && this.form.confirmarSenha != '') {
+               this.exibir = true
+               console.log('senha')
+               console.log(this.exibir)
+            }
          }
       }
    },
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -56,6 +56,8 @@ export default {
    components: {
       Upside
    },
+   methods:{
+   }
 }
 
 </script>
