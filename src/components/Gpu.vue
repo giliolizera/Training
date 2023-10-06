1f7c6ff681efc29b847d595b44c3cbc5fef1bcb9
giliolizera <guilhermegilioli93@gmail.com>
2023-10-06 10:43:41 -0300

    feat: rollback gpu and radio

--- src/components/Gpu.vue
+++ src/components/Gpu.vue
@@ -1,87 +1,90 @@
 <template>
-   <div class="text-slate-800 bg-gray-100 rounded dark:bg-slate-800 dark:text-white">
+   <div class="hidden lg:block">
+      <Upside/>
+   </div>
+   <div class="lg:hidden">
+      <UpSideMenu/>
+   </div>
+   <div class="h-screen text-slate-800 bg-gray-100 rounded dark:bg-slate-800 dark:text-white">
       <div class="flex justify-start">
-         <button @click="exibir = false"
-            class="dark:bg-slate-900 bg-gray-200 dark:text-gray-100 ml-3 mt-3 font-medium py-2 px-5 mb-2 rounded-xl">
-            Voltar para Radio
-         </button>
+         <router-link to="/radio">
+            <button class="dark:bg-slate-900 bg:gray-200 dark:text-gray-100 ml-3 mt-3 font-medium py-2 px-5 mb-2 rounded-xl">
+               Voltar para Radio
+            </button>
+         </router-link>
       </div>
-      <div>
-         <div class="mx-auto w-full max-w-md">
-            <div class="mx-auto w-full grid rounded-md">
-               <button v-for="(plan, index) in plans" :key="index" @click="active = plan.name">
-                  <div
-                     :class="[active === plan.name ? 'dark:bg-slate-900 bg-gray-500 ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-slate-600 text-white' : 'bg-white text-black', 'mx-auto w-full max-w-md rounded-md px-5 py-4 mt-3']">
-                     <ul class="justify-between flex">
-                        <li class="text-left text-sm font-medium">
-                           {{ plan.name }}
-                        </li>
-                        <button v-if="active === plan.name" class="absolute pl-96">
-                           <CheckCircleIcon class="hidden sm:block h-8 w-8 mt-1 text-black-500" />
-                        </button>
-                     </ul>
-                     <ul>
-                        <li
-                           :class="[active === plan.name ? 'static text-left text-sm text-gray' : 'static text-left text-sm text-gray-600']">
-                           {{ plan.vram }} vRAM
-                           <span aria-hidden="true"> &middot; </span>
-                           {{ plan.price }} R$
-                        </li>
-                     </ul>
-                  </div>
-               </button>
-            </div>
+      <div class="mx-auto w-full max-w-md py-2">
+         <div class="mx-auto w-full grid rounded-md">
+            <button v-for="(plan, index) in plans" :key="index" @click="active = plan.name">
+               <div
+                  :class="[active === plan.name ? 'dark:bg-slate-900 bg-gray-500 ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-slate-600 text-white' : 'bg-white text-black', 'mx-auto w-full max-w-md rounded-md px-5 py-4 mt-3']">
+                  <ul class="justify-between flex">
+                     <li class="text-left text-sm font-medium">
+                        {{ plan.name }}
+                     </li>
+                     <button v-if="active === plan.name" class="absolute pl-96">
+                        <CheckCircleIcon class="hidden sm:block h-8 w-8 mt-1 text-black-500" />
+                     </button>
+                  </ul>
+                  <ul>
+                     <li
+                        :class="[active === plan.name ? 'static text-left text-sm text-gray' : 'static text-left text-sm text-gray-600']">
+                        {{ plan.vram }} vRAM
+                        <span aria-hidden="true"> &middot; </span>
+                        {{ plan.price }} R$
+                     </li>
+                  </ul>
+               </div>
+            </button>
          </div>
       </div>
    </div>
 </template>
 
+
 <script>
+
 export default {
    data: () => ({
-      props:{
-         exibir: Boolean,
-      },
       plans: [
          {
-            name: 'AMD Radeon RX 580',
+            name: 'AMD RX 580',
             vram: '4GB',
             price: '399,00',
          },
          {
-            name: 'Asus Nvidia GTX 1060',
-            vram: '6GB',
-            price: '499,00',
+            name: 'Nvidia GTX 1650',
+            vram: '4GB',
+            price: '799,00',
          },
          {
-            name: 'AMD Challenger RX 6600',
+            name: 'AMD RX 6600',
             vram: '8GB',
             price: '1199,00',
          },
          {
-            name: 'Gigabyte Nvidia RTX 4060 Ti',
+            name: 'Nvidia RTX 4060',
             vram: '8GB',
-            price: '2499,00',
+            price: '1959,00',
          },
          {
-            name: 'Saphire AMD RX 7800XT',
-            vram: '12GB',
-            price: '3799,00',
-         },
-         {
-            name: 'Rog Strix Nvidia RTX 4080',
-            vram: '16GB',
-            price: '7299,00',
+            name: 'AMD RX 7700',
+            vram: '8GB',
+            price: '3249,00',
          }
       ],
       active: -1,
-      exibir: true,
    }),
+   components: {
+      Upside, UpSideMenu
+   },
 }
 
 </script>
 
 <script setup>
 import { CheckCircleIcon } from "@heroicons/vue/24/outline";
-document.title = "Gpu - Training"
+import Upside from "./Upside.vue";
+import UpSideMenu from "./UpSideMenu.vue";
+document.title="Gpu - Training"
 </script>
--- src/components/Radio.vue
+++ src/components/Radio.vue
@@ -5,9 +5,9 @@
    <div class="lg:hidden">
       <UpSideMenu/>
    </div>
-   <div class="text-slate-800 bg-gray-100 rounded dark:bg-slate-800 dark:text-white">
+   <div class="h-screen text-slate-800 bg-gray-100 rounded dark:bg-slate-800 dark:text-white">
       <div class="mx-auto w-full max-w-md py-8">
-         <div v-if="!exibir" class="mx-auto w-full grid rounded-md">
+         <div class="mx-auto w-full grid rounded-md">
             <button v-for="(plan, index) in plans" :key="index" @click="active = plan.name">
                <div
                   :class="[active === plan.name ? 'dark:bg-slate-900 bg-gray-500 ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-slate-600 text-white' : 'bg-white text-black', 'mx-auto w-full max-w-md rounded-md px-5 py-4 mt-3']">
@@ -22,25 +22,22 @@
                   <ul>
                      <li
                         :class="[active === plan.name ? 'static text-left text-sm text-gray' : 'static text-left text-sm text-gray-600']">
-                        {{ plan.ram }} RAM
-                        <span aria-hidden="true"> &middot; </span>                        
-                        {{ plan.cpus }}
+                        {{ plan.ram }} {{ plan.cpus }}
                         <span aria-hidden="true"> &middot; </span>
-                        {{ plan.price }} R$
+                        {{ plan.disk }}
                      </li>
                   </ul>
                </div>
             </button>
+            <div class="flex justify-end">
+               <router-link to="/gpu">
+                  <button class="dark:bg-slate-900 bg:gray-200 dark:text-gray-100 mt-3 font-medium py-2 px-5 mb-2 rounded-xl">
+                     GPU
+                  </button>
+               </router-link>
+            </div>
          </div>
       </div>
-   </div>
-   <div class="flex justify-end">
-         <button @click="exibir = true" class="dark:bg-slate-900 bg-gray-200 text-black dark:text-gray-100 mt-3 font-medium py-2 px-5 rounded-xl">
-            GPU
-         </button>
-         <div v-if="exibir">
-            <Gpu />
-         </div>
    </div>
 </template>
 
@@ -49,49 +46,39 @@
 
 export default {
    data: () => ({
-      props:{
-         exibir: Boolean,
-      },
       plans: [
          {
-            name: 'Intel Core i5 3470 3.60GHz',
-            ram: '8GB',
-            cpus: '4/4 CPU',
-            price: '139,99',
-         },
-         {
-            name: 'Intel Core i7 4770K 3.90GHz',
+            name: 'Intel Core i7 3770K 3.2GHz',
             ram: '8GB',
             cpus: '4/8 CPU',
-            price: '269,99',
+            disk: '240GB SSD',
          },
          {
-            name: 'AMD Ryzen 5 5600G 4.40GHz',
+            name: 'AMD Ryzen 5 5600G 3.4GHz',
             ram: '16GB',
             cpus: '6/12 CPU',
-            price: '899,99',
+            disk: '480GB SSD',
          },
          {
-            name: 'AMD Ryzen 7 5700X 4.60GHz',
+            name: 'AMD Ryzen 7 5700X 3.8GHz',
             ram: '32GB',
             cpus: '8/16 CPU',
-            price: '1249,99',
+            disk: '1TB SSD',
          },
          {
-            name: 'AMD Ryzen 9 5950X 4.9GHz',
+            name: 'AMD Ryzen 9 5950X 4.6GHz',
             ram: '64GB',
-            cpus: '16/32 CPU',
-            price: '3299,99',
+            cpus: '12/24 CPU',
+            disk: '2TB SSD',
          },
          {
-            name: 'Intel Core i9 13900KS 6.0GHz',
-            ram: '64GB',
-            cpus: '24/32 CPU',
-            price: '4999,99',
+            name: 'Intel Core i9 13900KS 5.0GHz',
+            ram: '128GB',
+            cpus: '20/40 CPU',
+            disk: '4TB SSD',
          }
       ],
       active: -1,
-      exibir: false,
    }),
    components: {
       Upside, UpSideMenu
@@ -104,6 +91,5 @@ export default {
 import { CheckCircleIcon } from "@heroicons/vue/24/outline";
 import Upside from "./Upside.vue";
 import UpSideMenu from "./UpSideMenu.vue";
-import Gpu from "./Gpu.vue";
 document.title="Radio - Training"
 </script>
